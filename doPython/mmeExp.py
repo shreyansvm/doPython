2a18b0d8b55977aaabbd36971a1e8bb293f73842
shreyansvm <shreyans.mulkutkar@gmail.com>
2018-04-01 16:42:43 -0700

    MME class for each mme grpId

--- doPython/mmeExp.py
+++ doPython/mmeExp.py
@@ -1,5 +1,17 @@
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+# create a file handler
+handler = logging.FileHandler('loggerInfo.log')
+handler.setLevel(logging.INFO)
+# create a logging format
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+# add the handlers to the logger
+logger.addHandler(handler)
+
 '''
-MmeSeqFile is derived class of testStep class
+MME Class
 '''
 class MmeExp(object):
     def __init__(self, **kwargs):
@@ -7,13 +19,16 @@ class MmeExp(object):
         self.__dict__.update((k, v) for k, v in kwargs.iteritems() if k in allowed_keys)
         for key, value in kwargs.iteritems():
             if key not in allowed_keys:
+                logger.error("%s is not a valid variable of class MmeExp." % key)
                 raise ValueError("%s is not a valid variable of class MmeExp." % key)
         self.mmeSeqFile = "mmeGrp" + self.mmeGrp + "CmdSeq.txt"
         self.mmeSeqFileObj = open(self.mmeSeqFile,'w')
-        # TODO : Add a variable with IMSI range based on MME GrpId
+        # IMSI Range : 123456789010000 , 123456789020000 ..
+        self.mmeImsiRange = int("1234567890" + self.mmeGrp + "0000")
 
     def __str__(self):
-        return "MME Grp : %s,\n\t on SpawnId : %s\n" % (self.mmeGrp, self.spawnId)
+        # TODO : As more variables are added, update the return string
+        return "MME Grp : %s,\n\t on SpawnId : %s\n\t IMSI range : %s\n" % (self.mmeGrp, self.spawnId, self.mmeImsiRange)
 
     def printMmeMgExpress(self):
         print('MmeExp class object - ')
@@ -21,7 +36,7 @@ class MmeExp(object):
             print('%s : %s' % (key, value))
 
     def addTestStep(self, testStep, stepCmds=None, stepComment=None):
-        print('Adding testStep %s on MME Grp : %s , SpawnId : %s\n' % (testStep, self.mmeGrp, self.spawnId))
+        logger.info('Adding testStep %s on MME Grp : %s , SpawnId : %s\n' % (testStep, self.mmeGrp, self.spawnId))
         if stepComment:
             self.mmeSeqFileObj.write("# " + stepComment + "\n")
         if stepCmds:
@@ -30,8 +45,18 @@ class MmeExp(object):
         For each testStep, update corresponding verification objects/methods
         '''
 
+if __name__ == '__main__':
+    mmeGroups = []
 
-mmeGrp1 = MmeExp(mmeGrp='1', spawnId='exp32')
-print(mmeGrp1)
-mmeGrp1.addTestStep('doAttach', stepCmds='proc attach group 1', stepComment="Attach IMSIs in group 1")
-mmeGrp1.addTestStep('doDetach', stepCmds='proc detach group 1', stepComment="Detach all IMSIs in group 1")
+    mmeGrp1 = MmeExp(mmeGrp='1', spawnId='exp32')
+    mmeGroups.append(mmeGrp1)
+    print(mmeGrp1)
+    mmeGrp1.addTestStep('doAttach', stepCmds='proc attach group 1', stepComment="Attach IMSIs in group 1")
+    mmeGrp1.addTestStep('doDetach', stepCmds='proc detach group 1', stepComment="Detach all IMSIs in group 1")
+
+    mmeGrp2 = MmeExp(mmeGrp='2', spawnId='exp32')
+    mmeGroups.append(mmeGrp2)
+    print(mmeGrp2)
+    mmeGrp2.addTestStep('doAttach', stepCmds='proc attach group 2', stepComment="Attach IMSIs in group 2")
+    mmeGrp2.addTestStep('mbCmd', stepCmds='proc mbcmd group 2', stepComment="MBCmd on all IMSIs in group 2")
+    mmeGrp2.addTestStep('doDetach', stepCmds='proc detach group 2', stepComment="Detach all IMSIs in group 2")
