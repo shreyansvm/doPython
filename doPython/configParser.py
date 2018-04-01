bb5a52105036fa0759974a1ff8b1c62675d6dde3
shreyansvm <shreyans.mulkutkar@gmail.com>
2018-03-31 19:34:24 -0700

    Transform an XML file using XSLT in Python. Used xltproc

--- doPython/configParser.py
+++ doPython/configParser.py
@@ -21,7 +21,30 @@ print("------------------------- XML --------------------------------")
 xmlRoot = etree.parse(xmlPath)
 print("XML Root - ",xmlRoot)
 
+xmlTemplate = "/tmp/"+str(uuid.uuid4())
+print(etree.tostring(xmlRoot,
+        pretty_print=True,
+        xml_declaration=True, encoding='US-ASCII').decode(), file=open(xmlTemplate, "w"))
+
 print("------------------------- transform --------------------------------")
 # transform xml with xslt
-transRoot = transform(xmlRoot)
-print("XML transRoot - ",transRoot)
+# transRoot = transform(xmlRoot)
+# print("XML transRoot - ",transRoot)
+
+# OR ----
+
+outfile = '/tmp/config_file_'
+
+# xsltproc is a command line tool for applying XSLT stylesheets to XML documents
+# http://xmlsoft.org/XSLT/xsltproc2.html
+# Synatx : xsltproc <XSL file> <XML Topo file> > <Output config file>
+cmd = "xsltproc {} {} > {}".format(xslPath, xmlTemplate, outfile)
+os.system(cmd)
+
+try:
+	# delete the temporary XML Topo template
+    os.remove(xmlTemplate)
+except OSError:
+    print('---- OSError ----')s
+
+print("------------------------- done --------------------------------")
